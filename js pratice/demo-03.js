695c408df6522080a5621a7653fcf1e7eb1bd75b
Rohith <57309698+rohith-karra@users.noreply.github.com>
2021-11-10 09:47:28 +0530

    Add files via upload

---
+++ js pratice/demo-03.js
@@ -0,0 +1,12 @@
+let amt = parseFloat(prompt('Enter principle amount: ' ,10000));
+let rt  = parseFloat(prompt('Enter rate of interest: ',5.5));
+let dur = parseInt(prompt('Enter duration in months: ',36));
+
+
+if (amt < 10000 || amt > 10000000) {
+    alert("Amount is outside the range of 10,000 <-> 1,00,00,00,000");
+}
+
+if (rt < 2 || rt > 20 ){
+    alert ("Rate of interest is ");
+}
